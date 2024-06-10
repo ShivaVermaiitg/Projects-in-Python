888d5a16f4e0fdcb38964750539f8e9ea7c9dee5
Shiva Verma <sv4383411@gmail.com>
2024-06-10 14:26:36 +0530

    Add files via upload

---
+++ Project1-100.py
@@ -0,0 +1,11 @@
+ # Greet your user
+print("Welcome to Band Baja Name Generator")
+
+ # Ask the user for their favorite pet name.
+pet = input("What is your favorite Pet name?\n")
+ # Ask the user for their favorite color.
+color = input("What is your favorite color?\n")
+
+ # Combine the name of their pet name and color to make their band name!
+print("Your Band Baja Name can be " +pet+ " " +color)
+ # Be sure the input cursor shows on a new line after the user hits enter!
